d36cbadd36b17e998cb433cf9ffa57646ce11087
Aashna-Verma <aashna.verma@outlook.com>
2022-12-29 14:54:06 -0500

    begining of redesign

--- src/nav.js
+++ src/nav.js
@@ -8,27 +8,28 @@ import './nav.css';
 
 const Nav = () => {
     return (
-        <form action=''>
+        <form action='' id="navi">
           <div></div>
-         
-          <div>
-            <input type="radio" id="search" name="nav" value="search" />
-            <img src={search} className="search-logo" alt="search logo"/>
-          </div>
           
           <div>
+            <div className="bar"></div>
             <input type="radio" id="home" name="nav" value="home" />
             <img src={home} className="home-logo" alt="home logo"/>
+            <span>home</span>
           </div>
           
           <div>
+            <div className="bar"></div>
             <input type="radio" id="friends" name="nav" value="friends" />
             <img src={friends} className="friends-logo" alt="friends logo"/>
+            <span>friends</span>
           </div>
 
           <div>
+            <div className="bar"></div>
             <input type="radio" id="messages" name="nav" value="messages" />
             <img src={messages} className="messages-logo" alt="messages logo"/>
+            <span>search</span>
           </div>
 
         </form>
